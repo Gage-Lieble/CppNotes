a2501fdb975b62e5acff236103ce1e94a24ef20a
Gage Lieble <gagelieble@hotmail.com>
2023-12-09 11:51:48 -0800

    maps

---
+++ exercises/map_char_counter.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+    string str = "Hello Gage";
+    map<char,int> countMap;
+
+    for(int i=0; i < str.length(); i++){ // iterates through string
+        char letter = str[i];
+        cout << letter << endl;
+        if (countMap.find(letter) == countMap.end()){ // if the .find pointer matches the .end pointer, the key val doesn't exist, so you have to create it
+            countMap[letter] = 0;
+        }
+        countMap[letter]++;
+    }
+
+    for (auto itr = countMap.begin(); itr != countMap.end(); itr++){ // displays map
+        cout << itr->first << ": " << itr->second << endl;
+    };
+    return 0;
+}
